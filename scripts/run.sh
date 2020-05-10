06acdec8ea49a9b05324aa52a4e959c4f26aca21
d.chien <dangchien1910@gmail.com>
2020-05-10 23:53:19 +0800

    incremental update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-#cd "$(dirname "$0")"
+me="$(basename "$0")"
+mydir="$(dirname "$0")"
 
 # check if bundle is installed
 if ! [[ -x "$(command -v bundle)" ]]; then
-    echo 'ERROR: bundle is not installed.' >&2
-    exit 1
+  echo 'ERROR: bundle is not installed.' >&2
+  exit 1
 fi
 
 # check if we need to install new/updated packages
-if [[ $(bundle check | wc -l) -gt 1 ]]; then
-    bundle install
+if [[ ! $(bundle check) ]]; then
+  bundle install
 fi
 
-# serve the blog in incremental development mode
+echo "$me" "$mydir"
+
+# clean the build dir
 bundle exec jekyll clean
+
+# serve the blog in incremental development mode
 #bundle exec jekyll serve --incremental
 bundle exec jekyll serve --incremental --drafts
